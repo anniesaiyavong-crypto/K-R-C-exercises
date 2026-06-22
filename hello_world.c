03d16e50f571a55b3ab42111be6eafe69190e2a0
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-06-22 15:54:10 +0700

    hello world with C and inline assembly

---
+++ hello_world.c
@@ -0,0 +1,16 @@
+int main() {
+	const char msg[] = "mello, world\n";
+	__asm__(
+			"mov $1, %%rax\n"
+			"mov $1, %%rdi\n"
+			"mov %0, %%rsi\n"
+			"mov $13, %%rdx\n"
+			"syscall\n"
+			:
+			: "r"(msg)
+			: "%rax", "%rdi", "%rsi", "%rdx"
+	       );
+
+			return 0;
+}
+
