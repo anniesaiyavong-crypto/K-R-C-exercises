be76b68f007ab48f962b2be3540d9f79f07f2479
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-06-23 18:00:40 +0700

    hello world with basic input/output library

--- hello_world.c
+++ hello_world.c
@@ -1,16 +1,6 @@
+#include <stdio.h>
 int main() {
-	const char msg[] = "hello, world\n";
-	__asm__(
-			"mov $1, %%rax\n"
-			"mov $1, %%rdi\n"
-			"mov %0, %%rsi\n"
-			"mov $13, %%rdx\n"
-			"syscall\n"
-			:
-			: "r"(msg)
-			: "%rax", "%rdi", "%rsi", "%rdx"
-	       );
-
-			return 0;
+	printf("Hello, World!\n");
+	return 0;
 }
 
