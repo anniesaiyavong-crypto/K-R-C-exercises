1ffa3dbfb543f379b37c5d8d4bb7a3bf97225d18
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-06-25 09:04:38 +0700

    introduce float

--- fah_cel.c
+++ fah_cel.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;
     upper = 300;
-    step = 10;
+    step = 10.5;
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%3d %6d\n", fahr, celsius);
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.1f\t%6.2f\n", fahr, celsius);
         fahr = fahr + step;
     }
     return 0;
