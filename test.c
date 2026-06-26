882b7283c54873e5c3aaa45a3af6e438c2452f27
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-06-26 14:56:45 +0700

    commit

--- test.c
+++ test.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 int main() {
-    int c, nl;
-    nl = 0;
-    while ((c = getchar()) != EOF) {
-        if (c == '\n') {
-            ++nl;
-            printf("%d\n", nl);
-        }
-        return 0;
+   int c, nl, nw, nc, state;
+   state = OUT;
+   nl = nw = nc = 0;
+   while ((c = getchar()) != EOF) {
+      ++nc;
+      if (c == '\n') {
+            nl++;
+            if (c == ' ' || c == '\n' || c == '\t')
+                state = OUT;
+                else if (state == OUT) {
+                    state = IN;
+                    ++nw;
+                }
+                printf("%d %d %d\n", nl, nw, nc);
+      }
+      
+      return 0;
     }
 }
