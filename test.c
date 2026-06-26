477accbdb165f97d82b85e55c2c791e291f2dfc8
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-06-26 19:32:44 +0700

    commit

--- test.c
+++ test.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
-
 int main() {
-   int c, nl, nw, nc, state;
-   state = OUT;
-   nl = nw = nc = 0;
-   while ((c = getchar()) != EOF) {
-      ++nc;
-      if (c == '\n') {
-            nl++;
-            if (c == ' ' || c == '\n' || c == '\t')
-                state = OUT;
-                else if (state == OUT) {
-                    state = IN;
-                    ++nw;
-                }
-                printf("%d %d %d\n", nl, nw, nc);
-      }
-      
-      return 0;
-    }
+    int c, i, nwhite, nother;
+    int ndigit[10];
+
+    nwhite = nother = 0;
+
+    for (int i = 0; i < 10; ++i) 
+    ndigit[i] = 0;
+
+    while ((c = getchar()) != EOF);
+        if (c >= '0' && c <= '9')
+            ++ndigit[c - '0'];
+            else if (c == ' ' || c == '\n' || c == '\t')
+            ++nwhite;
+            else
+            ++nother;
+
+            printf("digits =");
+            for (i = 0; i < 10; ++i) {
+                printf("%d",ndigit[i]);
+            }
+            printf(", white space = %d, other = %d\n", nwhite, nother);
+
+    
 }
