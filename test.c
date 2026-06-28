823cc604bbac258f98ea5401bc606ea42dc29d6b
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-06-28 18:18:57 +0700

    commit

--- test.c
+++ test.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
+#define MAXLINE 1000
 
-int power(int m, int n);
+int getLine(char line[], int maxline);
+void copy(char to[], char from[]);
 
-int main()
-{
-    int i;
+int main() {
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
 
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d %d %d\n", i, power(2, i), power(-3, i));
-    }
-    return 0;
-}
-int power(int base, int n)
-    {
-        int i, p;  
-         p = 1;
-        for (i = 1; i <= n; i++)
-        {
-            p = p * base;
+    max = 0;
+    while ((len = getLine(line, MAXLINE)) > 0) {
+        if (len > max) {
+            max = len;
+            copy(longest, line);
         }
-        return p;
+        if (max > 0) {
+            printf("%s", longest);
+        }
+        return 0;
     }
+}
+
+int getLine(char s[], int lim) {
+    int c, i;
+    for( i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+     s[i] = c; 
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+void copy(char to[], char from[]) {
+    int i;
+    i = 0;
+    while((to[i] = from[i]) != '\0') {
+        ++i;
+    }
+}
