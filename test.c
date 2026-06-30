543222e3f9c80f570591055795be9d4b19557cfb
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-06-30 20:58:23 +0700

    commit

--- test.c
+++ test.c
@@ -2,40 +2,37 @@
 
 int htoi(char s[]);
 
-
 int htoi(char s[]) {
-	int i = 0;
-	int result = 0;
-	int digit_value;
+  int i = 0;
+  int result = 0;
+  int digit_value;
 
-	if(s[i] == '0') {
-		i++;
-		if((s[i] == 'x') || (s[i] == 'X')) {
-			i++;
-		}
-	}
-	while (s[i] != '\0') {
-		if((s[i] >= '0') && (s[i] <= '9')) {
-			digit_value = s[i] - '0';
-		} else if((s[i] >= 'a') && (s[i] <= 'f')) {
-			digit_value = s[i] - 'a' + 10;
-		} else if((s[i] >= 'A') && (s[i] <= 'F')) {
-			digit_value = s[i] - 'A' + 10;
-		} else {
-			break;
-		}
-		result = (result * 16) + digit_value;
-		i++;
-	}
-	return result;
+  if (s[i] == '0') {
+    i++;
+    if ((s[i] == 'x') || (s[i] == 'X')) {
+      i++;
+    }
+  }
+  while (s[i] != '\0') {
+    if ((s[i] >= '0') && (s[i] <= '9')) {
+      digit_value = s[i] - '0';
+    } else if ((s[i] >= 'a') && (s[i] <= 'f')) {
+      digit_value = s[i] - 'a' + 10;
+    } else if ((s[i] >= 'A') && (s[i] <= 'F')) {
+      digit_value = s[i] - 'A' + 10;
+    } else {
+      break;
+    }
+    result = (result * 16) + digit_value;
+    i++;
+  }
+  return result;
 }
 
 int main() {
-	printf("0x1A3 ➔ %d\n", htoi("0x1A3"));
-    printf("0XFF  ➔ %d\n", htoi("0XFF")); 
-    printf("f     ➔ %d\n", htoi("f"));
+  printf("0x1A3 ➔ %d\n", htoi("0x1A3"));
+  printf("0XFF  ➔ %d\n", htoi("0XFF"));
+  printf("f     ➔ %d\n", htoi("f"));
 
-	return 0;
+  return 0;
 }
-
-
