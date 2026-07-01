3ace10b6a5de480a569754cf70919638c4713be6
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-07-01 16:25:57 +0700

    E 2-5 done

--- test.c
+++ test.c
@@ -1,43 +1,26 @@
 #include <stdio.h>
-// squeeze function
-void sq(char s1[], char s2[]) {
 
-  // i for s1, j for s2, k for the leftover
-  int i, j, k;
+int scan(char text[],char blacklist[]);
 
-  k = 0;
-  //read s1
-  for (i = 0; s1[i] != '\0'; i++) {
-    //read s2
-    for (j = 0; s2[j] != '\0'; j++) {
-      // break after they matched
-     if (s1[i] == s2[j]) {
-        break;
+
+
+int scan(char t[],char b[]) {
+  int i, j, s;
+
+  for(i = 0; t[i] != '\0'; i++) {
+    for(j = 0; b[j] != '\0'; j++) {
+      if(t[i] == b[j]) {
+        return i;
+      }
      }
     }
-    // if j found \0 its job is done
-    if (s2[j] == '\0') {
-      // push the leftover to s1 and count with k
-      s1[k++] = s1[i];
-    }
-  }
-  // k is done after \0
-  s1[k] = '\0';
+  return -1;
 }
 
-// main function
 int main() {
+  char text[] = "e sam see";
+  char blacklist[] = "a";
 
-  // data
-  char text[] = "babcock";
-  char blacklist[] = "bc";
-
-  // call the function
-  sq(text, blacklist);
-
-  // print
-  printf("result: %s\n", text);
+  printf("result = %d\n", scan(text, blacklist));
 }
-
-// result is aok
 
