97b1b0663b80d7d6fd7c3c6a4aa358d763a6ddee
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-07-02 15:14:24 +0700

    Exercise 2-7. Write a function invert(x,p,n) that returns x with the n bits that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged

--- test.c
+++ test.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-unsigned setbit(unsigned int x, int p, int n,unsigned int y) {
-return (x & ~(~(~0 << n) << (p + 1 - n))) | ((y & ~(~0 << n)) << (p + 1 - n));
+unsigned setbit(unsigned int x, int p, int n) {
+ return x ^ (~(~0 << n) << (p + 1 - n));
 }
 
 int main() {
   unsigned int x = 0xFF;
-  unsigned int y = 1;
   int p = 4;
   int n = 3;
-  printf("%d\n", setbit(n, x, p, y));
+  printf("%d\n", setbit(n, x, p));
 }
