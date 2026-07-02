3a8473b27319ed860dae9dd7fcf2803e6618cc3c
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-07-02 14:17:20 +0700

    Exercise 2-6. Write a function setbits(x,p,n,y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged

--- test.c
+++ test.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
 
-int scan(char text[],char blacklist[]);
-
-
-
-int scan(char t[],char b[]) {
-  int i, j, s;
-
-  for(i = 0; t[i] != '\0'; i++) {
-    for(j = 0; b[j] != '\0'; j++) {
-      if(t[i] == b[j]) {
-        return i;
-      }
-     }
-    }
-  return -1;
+unsigned setbit(unsigned int x, int p, int n,unsigned int y) {
+return (x & ~(~(~0 << n) << (p + 1 - n))) | ((y & ~(~0 << n)) << (p + 1 - n));
 }
 
 int main() {
-  char text[] = "e sam see";
-  char blacklist[] = "a";
-
-  printf("result = %d\n", scan(text, blacklist));
+  unsigned int x = 0xFF;
+  unsigned int y = 1;
+  int p = 4;
+  int n = 3;
+  printf("%d\n", setbit(n, x, p, y));
 }
-
