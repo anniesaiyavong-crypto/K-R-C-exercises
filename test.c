4d797f13d82170311b84230158f5e9567a434cbc
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-07-05 15:53:29 +0700

    shellsort

--- test.c
+++ test.c
@@ -1,48 +1,34 @@
 #include <stdio.h>
 
-void escape(char string[], char text[]);
+#define DE "%d"
+#define ST "%s"
+#define NT "\n"
+
+void shellsort(int v[], int n) {
+  int gap, i, j, temp;
+
+  for (gap = n/2; gap > 0; gap /= 2)
+    for (i = gap; i < n; i++)
+      for (j = i - gap; j >= 0 && v[j] > v[j + gap]; j -= gap) {
+        temp = v[j];
+        v[j] = v[j + gap];
+        v[j + gap] = temp;
+      }
+}
 
 int main() {
-  char cat[] = "hello \n \t";
-  char esc[100] = "";
+  int num[] = {1, 5, 7, 8, 2, 9, 6, 10, 3, 4};
+  int n = (sizeof(num) / sizeof(num[0]));
+  int i;
 
-  escape(esc, cat);
+  shellsort(num, n);
+  printf("[");
+  for (i = 0; i < n; i++) {
+   printf(DE, num[i]);
+   if (i < n - 1)
+     printf(", ");
 
-  printf("%s\n", esc);
-    return 0;
-}
-
-void escape(char s[], char t[]) {
-  int i, j;
-  j = 0;
-  char c;
-
-  for (i = 0; t[i] != '\0'; i++) {
-   c = t[i];
-
-   switch (c) {
-     case '\n' :
-       s[j++] = '\\';
-       s[j++] = 'n';
-        break;
-     case '\t' :
-       s[j++] = '\\';
-       s[j++] = 't';
-       break;
-     default :
-       s[j++] = c;
-       break;
-    }
   }
-  s[j] = '\0';
+  printf("]"NT);
+ return 0;
 }
-
-
-
-
-
-
-
-
-
-
