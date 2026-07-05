d0e52e6b7fd1541fdd5068fa9c6ae1a8e73e9081
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-07-05 18:04:53 +0700

    Exercise 3-3. Write a function expand(s1,s2) that expands shorthand notations like a-z in the string s1 into the equivalent complete list abc...xyz in s2. Allow for letters of either case and digits, and be prepared to handle cases like a-b-c and a-z0-9 and -a-z. Arrange that a leading or trailing - is taken literally

--- test.c
+++ test.c
@@ -2,33 +2,50 @@
 
 #define DE "%d"
 #define ST "%s"
-#define NT "\n"
+#define NL "\n"
+#define END '\0'
 
-void shellsort(int v[], int n) {
-  int gap, i, j, temp;
+void expand(char s1[], char s2[]) {
+  int i = 0;
+  int j = 0;
+  char c;
 
-  for (gap = n/2; gap > 0; gap /= 2)
-    for (i = gap; i < n; i++)
-      for (j = i - gap; j >= 0 && v[j] > v[j + gap]; j -= gap) {
-        temp = v[j];
-        v[j] = v[j + gap];
-        v[j + gap] = temp;
+  while (s1[i] != END) {
+    if (i > 0 && s1[i] == '-' && s1[i + 1] != END) {
+      char start = s1[i - 1];
+      char end = s1[i + 1];
+
+      for (c = start + 1; c <= end; c++) {
+        s2[j++] = c;
       }
+      i += 2;
+    }
+    else {
+      s2[j++] = s1[i];
+      i++;
+    }
+  }
+  s2[j] = END;
 }
 
 int main() {
-  int num[] = {1, 5, 7, 8, 2, 9, 6, 10, 3, 4};
-  int n = (sizeof(num) / sizeof(num[0]));
-  int i;
+  char s1[] = "0-9";
+  char s2[100] = "";
+  expand(s1, s2);
 
-  shellsort(num, n);
-  printf("[");
-  for (i = 0; i < n; i++) {
-   printf(DE, num[i]);
-   if (i < n - 1)
-     printf(", ");
+  printf(ST"\n", s2);
 
-  }
-  printf("]"NT);
- return 0;
+  return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
