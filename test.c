1883cf6f145d9b66e06f58809bc708f2c7f47466
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-07-06 10:22:36 +0700

    int to string

--- test.c
+++ test.c
@@ -1,41 +1,58 @@
 #include <stdio.h>
+#include <string.h>
+
 
 #define DE "%d"
 #define ST "%s"
 #define NL "\n"
 #define END '\0'
+// function prototypes
+void reverse(char text[]);
+void itoa(int n, char s[]);
 
-void expand(char s1[], char s2[]) {
-  int i = 0;
-  int j = 0;
-  char c;
 
-  while (s1[i] != END) {
-    if (i > 0 && s1[i] == '-' && s1[i + 1] != END) {
-      char start = s1[i - 1];
-      char end = s1[i + 1];
 
-      for (c = start + 1; c <= end; c++) {
-        s2[j++] = c;
-      }
-      i += 2;
-    }
-    else {
-      s2[j++] = s1[i];
-      i++;
-    }
-  }
-  s2[j] = END;
-}
-
+// main
 int main() {
-  char s1[] = "0-9";
-  char s2[100] = "";
-  expand(s1, s2);
+  int n = 189002;
+  char s[100] = "";
+  itoa(n, s);
 
-  printf(ST"\n", s2);
+  printf(ST NL, s);
 
   return 0;
+}
+
+
+// int to string
+void itoa(int n, char s[]) {
+  int i, sigh;
+  if ((sigh = n) < 0) {
+    n = -n;
+  }
+  i = 0;
+
+  do {
+    s[i++] = n % 10 + '0';
+  }
+  while ((n /= 10) > 0);
+  if (sigh < 0) {
+    s[i++] = '-';
+  }
+  s[i] = END;
+  reverse(s);
+}
+// reverse
+void reverse(char s[]) {
+  int i, j;
+  char c;
+
+  for (i = 0, j = strlen(s) - 1;  i < j; i++, j--) {
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+
 }
 
 
