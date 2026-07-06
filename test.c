a825282b5df4c0bec09353cd15b8889b7cb7c46a
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-07-06 20:42:08 +0700

    Exercise 3-6. Write a version of itoa that accepts three arguments instead of two. The third argument is a minimum field width; the converted number must be padded with blanks on the left if necessary to make it wide enough

--- test.c
+++ test.c
@@ -8,44 +8,53 @@
 #define END '\0'
 // function prototypes
 void reverse(char text[]);
-void base_conversion(int n, char s[], int b);
+void itoa(int n, char s[], int w);
 
 
 
 // main
 int main() {
-  int n = 255;
-  int b = 16;
+  int n = -123;
   char s[100] = "";
+  int width = 10;
+  itoa(n, s, width);
 
-  base_conversion(n, s, b);
   printf(ST NL, s);
-
 
   return 0;
 }
 
 
-// base_conversion function
-void base_conversion(int n, char s[], int b) {
-  int i = 0;
-  int sign = n;
-  char digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+// int to string
+void itoa(int n, char s[], int w) {
+  int i, sign;
+  sign = n;
+  i = 0;
+  int le;
+
+
 
   do {
-    int rem = n % b;
+    int rem = n % 10;
     if (rem < 0)
-      rem = -rem;
-    s[i++] = digits[rem];
-    n /= b;
+      s[i++] = '0' - rem;
+    else
+      s[i++] = '0' + rem;
+    n /= 10;
   }
 
   while (n != 0);
   if (sign < 0) {
     s[i++] = '-';
   }
+
+  while (i < w) {
+    s[i++] = ' ';
+  }
   s[i] = END;
+
   reverse(s);
+
 }
 // reverse
 void reverse(char s[]) {
@@ -59,15 +68,3 @@ void reverse(char s[]) {
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
