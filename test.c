e423c16b83039f88f4478637f5511f919575747f
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-07-07 22:56:49 +0700

    Exercise 4-2. Extend atof to handle scientific notation of the form  123.45e-6  where a floating-point number may be followed by e or E and an optionally signed exponent

--- test.c
+++ test.c
@@ -13,25 +13,31 @@ double atofl(char s[]);
 
 // main
 int main() {
-  char s[100] = "123";
+  char s[100] = "123e10";
 
   printf("%f" NL, atofl(s));
   return 0;
 }
+// ASCII to Float
 double atofl(char s[]) {
   double val, power;
-  int i, sign;
+  int i, sign, expo_sign, expo_val;
 
+
+  // clear the space
   for (i = 0; isspace(s[i]); i++) {
     ;
   }
+  // check sign
   sign = (s[i] == '-') ? -1 : 1;
   if (s[i] == '+' || s[i] == '-') {
     i++;
   }
+
   for (val = 0.0; isdigit(s[i]); i++) {
     val = 10.0 * val + (s[i] - '0');
   }
+
   if (s[i] == '.') {
     i++;
   }
@@ -39,7 +45,30 @@ double atofl(char s[]) {
     val = 10.0 * val + (s[i] = '0');
     power *= 10;
   }
-  return sign * val / power;
+  val = sign * val / power;
+
+  if (s[i] == 'e' || s[i] == 'E') {
+    i++;
+
+    expo_sign = (s[i] == '-') ? -1 : 1;
+
+    if (s[i] == '+' || s[i] == '-') {
+      i++;
+    }
+    for (expo_val = 0; isdigit(s[i]); i++) {
+      expo_val = 10 * expo_val + (s[i] - '0');
+    }
+
+    for (int k = 0; k < expo_val; k++) {
+      if (expo_sign == -1) {
+        val /= 10.0;
+      }
+      else {
+        val *= 10;
+      }
+    }
+  }
+  return val;
 }
 
 
