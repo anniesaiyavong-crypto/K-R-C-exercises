0c2e29fb7b1549ca1cef0e33631ec8212781bb42
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-08-13 16:44:37 +0700

    make it usable for if-else
    using do-while(0)

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#define swap(t, x, y) { \
+#define swap(t, x, y) do {\
 t temp = x;\
 x = y;\
 y = temp;\
-}
+} while(0)
 
 int main() {
     int x = 2;
@@ -12,7 +12,8 @@ int main() {
     printf("before\nX = ""%d\n", x);
     printf("Y = ""%d\n", y);
 
-    swap(int, x, y);
+    if (x < y)
+        swap(int, x, y);
 
     printf("After\nX = ""%d\n", x);
     printf("Y = ""%d\n", y);
