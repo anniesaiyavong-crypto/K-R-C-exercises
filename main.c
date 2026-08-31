3a9aadfceb2a719a59cc1da54611136c2aacb9ab
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-08-31 08:53:31 +0700

    shorter version for string copy
    with pointer

--- main.c
+++ main.c
@@ -1,13 +1,5 @@
-// copy t to s, array version
-void string_cpy(char *s, char *t) {
-  int i = 0;
-  while ((s[i] = t[i]) != '\0')
-    i++;
-}
-// copy, pointer version
+// string copy, shorter pointer version
 void string_cpy_p(char *s, char *t) {
-  while ((*s = *t) != '\0') {
-    s++;
-    t++;
-  }
+  while ((*s++ = *t++) != '\0')
+      ;
 }
