1fea12b62a8c0ff2d25bf02b26e67b0de2d45065
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-08-31 16:20:34 +0700

    Exercise 5-4. Write the function strend(s,t), which returns 1 if the string t occurs at the end of the string s, and zero otherwise

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
-void strcat(char *s, char *t) {
-    // find \0
+#include <stdio.h>
+#include <string.h>
+// if t occurs at the end of s, return 1, 0 otherwise
+int strend(char *s, char *t) {
+    char *t_start_ptr = t;
+    // check the length
+    if (strlen(s) < strlen(t))
+        return 0;
+  // move both to '\0'
     while (*s)
         s++;
-    // copy loop
-    while (*s++ = *t++)
-        ;
+    while (*t)
+        t++;
+    // move back
+    while (*s == *t) {
+        s--;
+        t--;
+    }
+    // check
+    if (++t == t_start_ptr)
+        return 1;
+
+    return 0;
+}
+
+int main() {
+    char s[] = {"banana"};
+    char t[] = {"na"};
+
+    printf("%d\n", strend(s, t));
+
 }
