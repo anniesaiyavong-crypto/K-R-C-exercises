ef16385e78bfc7d230042cd26a8c78957b939795
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-01 11:04:02 +0700

    Exercise 5-6, add getline

--- main.c
+++ main.c
@@ -1,39 +1,17 @@
 #include <stdio.h>
-void str_cpy(char *s, char *t, int n) {
-    // copy until n become 0 or no char left in t
-  while (n > 0 && *t != '\0') {
-    *s++ = *t++;
-    n--;
-  }
-  // end with \0
-  if (n > 0)
-    *s = '\0';
-}
-void str_cat(char *s, char *t, int n) {
-    // move to \0
-  while (*s)
-    s++;
-    // copy to its end
-  while (n > 0 && *t != '\0') {
-    *s++ = *t++;
-    n--;
-  }
+int get_line(char *s, int lim) {
+    int c;
+    char *s_start_ptr = s;
+    // copy input loop
+  while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+    *s++ = c;
+    // include newline
+  if (c == '\n') {
+    *s++ = c;
 
+  }
+  // apply '\0'
   *s = '\0';
-}
-
-int str_cmp(char *s, char *t, int n) {
-    // compare s and t until n is 0
-    for (; n > 0 && *s == *t; s++, t++, n--)
-        if (*s == '\0')
-            return 0;
-
-    return (n == 0) ? 0 : (*s - *t);
-}
-int main() {
-  char s[] = {"II000"};
-  char t[] = {"IIIII"};
-  int n = 2;
-
-  printf("%d\n", str_cmp(s, t, n));
+  // return string length
+  return s - s_start_ptr;
 }
