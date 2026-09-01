d877cb387a1831406644b432f90af86b4c7c95cc
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-01 22:03:44 +0700

    add reverse and itoa, pointer version

--- main.c
+++ main.c
@@ -1,18 +1,44 @@
-#include <ctype.h>
-int atoi(char *s) {
-  int n = 0;
-  int sign;
-// skip white space
-  while (isspace(*s))
-      s++;
-
-  sign = (*s == '-') ? -1 : 1;
-
-  if (*s == '+' || *s == '-')
-    s++;
-
-  while (isdigit(*s)) {
-    n = 10 * n + (*s++ - '0');
+#include <string.h>
+// reverse string s
+void reverse(char *s) {
+  // Check for null or empty string
+  if (s == NULL || *s == '\0') {
+    return;
   }
-  return sign * n;
+  // point to the last character before '\0'
+  char *s_start = s;
+  char *end = s + strlen(s) - 1;
+  char temp;
+
+  // Swap characters until the pointers meet in the middle
+  while (s_start < end) {
+    temp = *s_start;
+    *s_start = *end;
+    *end = temp;
+
+    // Move the pointers toward each other
+    s_start++;
+    end--;
+  }
 }
+
+// convert n to characters in s
+void itoa(int n, char *s) {
+  int sign;
+  char *s_start = s;
+
+  // save sign & make n positive
+  if ((sign = n) < 0)
+    n = -n;
+  // generate digits
+  do {
+    *s++ = n % 10 + '0';
+  } while ((n /= 10) > 0);
+  // negative case
+  if (sign < 0)
+    *s++ = '-';
+  // end with null
+  *s = '\0';
+  reverse(s_start);
+}
+
