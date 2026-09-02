d4c61779d352a52d7805cec0ae6ae78f3e0693e6
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-02 11:17:56 +0700

    Exercise 5-6. Rewrite appropriate programs from earlier chapters and exercises with pointers instead of array indexing. Good possibilities include getline (Chapters 1 and 4), atoi, itoa, and their variants (Chapters 2, 3, and 4), reverse (Chapter 3), and strindex and getop (Chapter 4)

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
+//--------------------------------------
 // token
 #define NUMBER '0'
-
 int getch(void);
 void ungetch(int);
 
@@ -36,3 +37,136 @@ int getop(char *s) {
 
   return NUMBER;
 }
+//--------------------------------------
+// return the first index of t that occurred in s
+int strindex(char *s, char *t) {
+    char *s_ptr, *p1, *p2;
+    // return if t is empty
+    if (*t == '\0')
+        return 0;
+
+    for (s_ptr = s; *s_ptr != '\0'; s_ptr++) {
+        p1 = s_ptr;
+        p2 = t;
+        // checking loop
+        while (*p1 != '\0' && *p2 != '\0' && *p1 == *p2) {
+            p1++;
+            p2++;
+        }
+        // return index
+        if (*p2 == '\0')
+            return s_ptr - s;
+    }
+    return -1;
+}
+//--------------------------------------
+// string to double
+double atof(char *s) {
+    double val = 0.0, power = 1.0;
+    int sign;
+
+    // skip spaces
+    while (isspace(*s))
+        s++;
+
+    // save sign
+    sign = (*s == '-') ? -1 : 1;
+    if (*s == '+' || *s == '-')
+        s++;
+
+    // convert
+    while (isdigit(*s))
+        val = 10.0 * val + (*s++ - '0');
+
+    // skip decimal point
+    if (*s == '.')
+        s++;
+
+    // convert fraction
+    while (isdigit(*s)) {
+        val = 10.0 * val + (*s++ - '0');
+        power *= 10.0;
+    }
+
+    return sign * val / power;
+}
+//--------------------------------------
+// reverse string s
+void reverse(char *s) {
+    // Check for null or empty string
+    if (s == NULL || *s == '\0') {
+        return;
+    }
+    // point to the last character before '\0'
+    char *s_start = s;
+    char *end = s + strlen(s) - 1;
+    char temp;
+
+    // Swap characters until the pointers meet in the middle
+    while (s_start < end) {
+        temp = *s_start;
+        *s_start = *end;
+        *end = temp;
+
+        // Move the pointers toward each other
+        s_start++;
+        end--;
+    }
+}
+//--------------------------------------
+// convert n to characters in s
+void itoa(int n, char *s) {
+    int sign;
+    char *s_start = s;
+
+    // save sign & make n positive
+    if ((sign = n) < 0)
+        n = -n;
+    // generate digits
+    do {
+        *s++ = n % 10 + '0';
+    } while ((n /= 10) > 0);
+    // negative case
+    if (sign < 0)
+        *s++ = '-';
+    // end with null
+    *s = '\0';
+    reverse(s_start);
+}
+//--------------------------------------
+// string to int
+int atoi(char *s) {
+    int n = 0;
+    int sign;
+    // skip white space
+    while (isspace(*s))
+        s++;
+    // save sing
+    sign = (*s == '-') ? -1 : 1;
+
+    if (*s == '+' || *s == '-')
+        s++;
+    // generate integer value
+    while (isdigit(*s)) {
+        n = 10 * n + (*s++ - '0');
+    }
+    return sign * n;
+}
+//--------------------------------------
+int get_line(char *s, int lim) {
+    int c;
+    char *s_start_ptr = s;
+    // copy input loop
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+        *s++ = c;
+    // include newline
+    if (c == '\n') {
+        *s++ = c;
+
+    }
+    // apply '\0'
+    *s = '\0';
+    // return string length
+    return s - s_start_ptr;
+}
+//--------------------------------------
