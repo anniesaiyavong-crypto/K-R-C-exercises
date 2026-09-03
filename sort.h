36d86bab3a41129ff39e122fcbb8d22c2e2d043e
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-03 11:58:52 +0700

    clean up

--- sort.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define MAXSTORE 500000
-void writelines(char *lineptr[], int nlines);
-int readlines(char *lineptr[], int maxlinesม, char *store);
-void qsort(char *lineptr[], int left, int right);
-int get_line(char *, int);
