696245f75840dfca2607e28c5b6055d2400741de
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-03 14:36:06 +0700

    Exercise 5-8. There is no error checking in day_of_year or month_day. Remedy this defect

--- main.c
+++ main.c
@@ -4,7 +4,20 @@ static char daytab[2][13] = {
 // day_of_year: set day of year from month & day
 int day_of_year(int year, int month, int day) {
   int i, leap;
+
+  // error checking: year
+  if (year < 1)
+    return -1;
+
   leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
+  // error checking: month
+  if (month < 1 || month > 12)
+    return -1;
+  // error checking: day
+  if (day < 1 || day > daytab[leap][i])
+    return -1;
+  // calculate day
   for (i = 1; i < month; i++)
     day += daytab[leap][i];
   return day;
@@ -14,6 +27,14 @@ void month_day(int year, int yearday, int *month_ptr, int *day_ptr) {
   int i, leap;
 
   leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+  // check for valid yearday
+  int max_days = leap ? 366 : 365;
+  if (yearday > max_days) {
+    *month_ptr = -1;
+    *day_ptr = -1;
+    return;
+  }
+
   for (i = 1; yearday > daytab[leap][i]; i++)
     yearday -= daytab[leap][i];
 
