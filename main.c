f3ac4a649fd6eee8b45bfb9e5c37f9322e2e3c91
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-03 14:53:00 +0700

    month name function
    An array of pointers to constants

--- main.c
+++ main.c
@@ -1,43 +1,7 @@
-static char daytab[2][13] = {
-    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
-// day_of_year: set day of year from month & day
-int day_of_year(int year, int month, int day) {
-  int i, leap;
+char *month_name(int n) {
+  static char *name[] = {"Illegal month", "Jan",  "Feb",  "Mar", "Apr",
+                         "May",           "June", "July", "Aug", "Sep",
+                         "Oct",           "Nov",  "Dec"};
 
-  // error checking: year
-  if (year < 1)
-    return -1;
-
-  leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-
-  // error checking: month
-  if (month < 1 || month > 12)
-    return -1;
-  // error checking: day
-  if (day < 1 || day > daytab[leap][i])
-    return -1;
-  // calculate day
-  for (i = 1; i < month; i++)
-    day += daytab[leap][i];
-  return day;
-}
-// month_day: set month, day from day of year
-void month_day(int year, int yearday, int *month_ptr, int *day_ptr) {
-  int i, leap;
-
-  leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-  // check for valid yearday
-  int max_days = leap ? 366 : 365;
-  if (yearday > max_days) {
-    *month_ptr = -1;
-    *day_ptr = -1;
-    return;
-  }
-
-  for (i = 1; yearday > daytab[leap][i]; i++)
-    yearday -= daytab[leap][i];
-
-  *month_ptr = i;
-  *day_ptr = yearday;
+  return (n < 1 || n > 12) ? name[0] : name[n];
 }
