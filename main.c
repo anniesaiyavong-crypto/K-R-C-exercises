69a8c90409886b6480f39340e5a44832ccb9e910
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-04 09:48:21 +0700

    pattern-finding program version 2, support arguments
    -n: print line with sequence number
    -x: invert the program
    combine flag is allowed
    can be written as -xn

--- main.c
+++ main.c
@@ -3,17 +3,53 @@
 
 #define MAXLINE 1000
 int get_line(char *, int);
-
+// find: print lines that match pattern from 1st arg
 int main(int argc, char *argv[]) {
   char line[MAXLINE];
-  int found = 0;
-  if (argc != 2)
-    printf("Usage: find pattern\n");
+  long lineno = 0;
+  int c, except = 0, number = 0, found = 0;
+
+  while (--argc > 0 && (*++argv)[0] == '-')
+    while ((c = *++argv[0]))
+      switch (c) {
+      case 'x':
+        except = 1;
+        break;
+      case 'n':
+        number = 1;
+        break;
+      default:
+        printf("find: illegal option %c\n", c);
+        argc = 0;
+        found = -1;
+        break;
+      };
+  if (argc != 1)
+    printf("Usage: find -x -n pattern\n");
   else
-    while (get_line(line, MAXLINE) > 0)
-      if (strstr(line, argv[1]) != NULL) {
+    while (get_line(line, MAXLINE) > 0) {
+      lineno++;
+      if ((strstr(line, *argv) != NULL) != except) {
+        if (number)
+          printf("%ld:", lineno);
         printf("%s", line);
         found++;
       }
+    }
   return found;
+}
+int get_line(char *s, int lim) {
+  int c;
+  char *s_start_ptr = s;
+  // copy input loop
+  while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+    *s++ = c;
+  // count newline
+  if (c == '\n') {
+    *s++ = c;
+  }
+  // end with NULL
+  *s = '\0';
+  // return string length
+  return s - s_start_ptr;
 }
