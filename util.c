caccdeaae2a4b70f8b97fd5d141b2b8e0237a598
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-08 08:04:57 +0700

    Exercise 6-1. Our version of getword does not properly handle underscores, string constants, comments, or preprocessor control lines. Write a better version

--- util.c
+++ util.c
@@ -2,6 +2,8 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+int getch(void);
+void ungetch(int c);
 
 // binsearch: find word in tab[0]... tab[n-1]
 int binsearch(char *word, struct key tab[], int n) {
@@ -24,24 +26,66 @@ int binsearch(char *word, struct key tab[], int n) {
 }
 // getword: get next word or character from input
 int getword(char *word, int lim) {
-  int c, getch(void);
-  void ungetch(int);
+  int c, d;
   char *w = word;
+
   // skip space
   while (isspace(c = getch()))
     ;
+
   if (c != EOF)
     *w++ = c;
-  if (!isalpha(c)) {
-    *w = '\0';
-    return c;
-  }
-  for (; --lim > 0; w++)
-    if (isalnum(*w = getch())) {
-      ungetch(*w);
-      break;
-    }
-  *w = '\0';
 
-  return word[0];
+  if (isalpha(c) || c == '_') {
+    for (; --lim > 0; w++) {
+      if (!isalnum(*w = getch()) && *w != '_') {
+        ungetch(*w);
+        break;
+      }
+    }
+    *w = '\0';
+    return word[0];
+  }
+
+  if (c == '"' || c == '\'') {
+    int quote = c;
+    for (; --lim > 0; w++) {
+      if ((*w = getch()) == '\\') {
+        if (--lim > 0)
+          *++w = getch();
+      } else if (*w == quote || *w == EOF) {
+        if (*w != EOF)
+          w++;
+        break;
+      }
+    }
+    *w = '\0';
+    return word[0];
+  }
+  if (c == '/') {
+    if ((d = getch()) == '*') {
+      while ((c = getch()) != EOF) {
+        if (c == '*') {
+          if ((c = getch()) == '/')
+            break;
+          else
+            ungetch(c);
+        }
+      }
+      return getword(word, lim);
+    } else if (d == '/') {
+      while ((c = getch()) != '\n' && c != EOF)
+        ;
+      return getword(word, lim);
+    } else {
+      ungetch(d);
+    }
+  }
+  if (c == '#') {
+    while ((c = getch()) != '\n' && c != EOF)
+      ;
+    return getword(word, lim);
+  }
+  *w = '\0';
+  return c;
 }
