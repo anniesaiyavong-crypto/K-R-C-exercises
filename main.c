df0437dec2de8df6cabc4696b09584db825dd31c
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-09 18:03:27 +0700

    add lookup function

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "common.h"
+#include <stdio.h>
+#include <string.h>
 // pointer table
 static struct nlist *hashtab[HASHSIZE];
 
@@ -10,4 +12,13 @@ unsigned hash(char *s) {
     hashval = *s + 31 * hashval;
 
   return hashval % HASHSIZE;
+}
+// lookup: look for s in hashtab
+struct nlist *lookup(char *s) {
+  struct nlist *np;
+
+  for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+    if (strcmp(s, np->name) == 0)
+      return np; // found
+  return NULL;   // not found
 }
