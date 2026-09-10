6d1bdc221e8140ded6a89b91a76c0c91f978aaf8
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-10 08:06:03 +0700

    add install function

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 // pointer table
 static struct nlist *hashtab[HASHSIZE];
@@ -16,9 +17,28 @@ unsigned hash(char *s) {
 // lookup: look for s in hashtab
 struct nlist *lookup(char *s) {
   struct nlist *np;
-
+  // search
   for (np = hashtab[hash(s)]; np != NULL; np = np->next)
     if (strcmp(s, np->name) == 0)
       return np; // found
   return NULL;   // not found
+}
+// install: put name and definition in hashtab
+struct nlist *install(char *name, char *defn) {
+  struct nlist *np;
+  unsigned hashval;
+
+  if ((np = lookup(name)) == NULL) { // not found
+    np = (struct nlist *)malloc(sizeof(*np));
+    if (np == NULL || (np->name = strdup(name)) == NULL)
+      return NULL;
+    hashval = hash(name);
+    np->next = hashtab[hashval];
+    hashtab[hashval] = np;
+  } else                    // already there
+    free((void *)np->defn); // free prev defn
+  if ((np->defn = strdup(defn)) == NULL)
+    return NULL;
+
+  return np;
 }
