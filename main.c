0d6ac7eb1934336bb72b1dcd12f33991f1b6c91d
anniesaiyavong-crypto <anniesaiyavong@gmail.com>
2026-09-12 11:03:05 +0700

    Exercise 7-3. Revise minprintf to handle more of the other facilities of printf

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 void minprintf(char *fmt, ...) {
   va_list ap; // pointer to unnamed argument in turn
   char *p, *sval;
-
+  unsigned uval;
   int ival;
   double dval;
+  void *pval;
 
   va_start(ap, fmt); // make ap point to 1st argument
   for (p = fmt; *p; p++) {
@@ -16,8 +17,34 @@ void minprintf(char *fmt, ...) {
     }
     switch (*++p) {
     case 'd':
+    case 'i':
       ival = va_arg(ap, int);
       printf("%d", ival);
+      break;
+    case 'u':
+      uval = va_arg(ap, unsigned int);
+      printf("%u", uval);
+      break;
+    case 'o':
+      uval = va_arg(ap, unsigned int);
+      printf("%o", uval);
+      break;
+    case 'x':
+      uval = va_arg(ap, unsigned int);
+      printf("%x", uval);
+    case 'X':
+      uval = va_arg(ap, unsigned int);
+      printf("%X", uval);
+    case 'c':
+      ival = va_arg(ap, int);
+      putchar(ival);
+      break;
+    case 'p':
+      pval = va_arg(ap, void *);
+      printf("%p", pval);
+      break;
+    case '%':
+      putchar('%');
       break;
     case 'f':
       dval = va_arg(ap, double);
